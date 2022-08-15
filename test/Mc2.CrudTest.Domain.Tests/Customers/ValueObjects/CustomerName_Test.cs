286e85128632efd9695b9f4bdcc679e1e2f6cdc9
Reza P <rpouya@hotmail.com>
2022-08-16 01:40:39 +0430

    Add Another Test

--- test/Mc2.CrudTest.Domain.Tests/Customers/ValueObjects/CustomerName_Test.cs
+++ test/Mc2.CrudTest.Domain.Tests/Customers/ValueObjects/CustomerName_Test.cs
@@ -1,4 +1,5 @@
 ﻿using Mc2.CrudTest.Domain.Customers;
+using Mc2.CrudTest.Domain.Customers.ValueObjects;
 using Mc2.CrudTest.Domain.Tests.MoqObjects;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,13 @@ namespace Mc2.CrudTest.Domain.Tests.Customers.ValueObjects
         {
             Assert.Null(_customer.Name.FirstName);
             Assert.Null(_customer.Name.LastName);
+        }
+
+        [Fact]
+        public void Should_throw_Exception_if_customer_fname_is_empty()
+        {
+            Action act = () => new CustomerName("", "Pouya");
+            Assert.Throws<CustomerException>(act);
         }
     }
 }
