a8b5457db2e97e8bbaeef5abe77f167333be626e
Reza P <rpouya@hotmail.com>
2022-08-16 01:20:14 +0430

    Add first failing Test

--- test/Mc2.CrudTest.Domain.Tests/Customers/Customer_Test.cs
+++ test/Mc2.CrudTest.Domain.Tests/Customers/Customer_Test.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Mc2.CrudTest.Domain.Tests.Customers
 {
     public class Customer_Test
     {
+        [Fact]
+        public void WeShouldHaveCustomer()
+        {
+            Assert.NotNull(_customer);
+        }
     }
 }
