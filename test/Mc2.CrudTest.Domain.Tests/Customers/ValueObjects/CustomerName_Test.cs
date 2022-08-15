0f3053f6a296b4d97c5d026320f5ed92f577766f
Reza P <rpouya@hotmail.com>
2022-08-16 01:36:06 +0430

    Add CustomerName Test

---
+++ test/Mc2.CrudTest.Domain.Tests/Customers/ValueObjects/CustomerName_Test.cs
@@ -0,0 +1,27 @@
+﻿using Mc2.CrudTest.Domain.Customers;
+using Mc2.CrudTest.Domain.Tests.MoqObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mc2.CrudTest.Domain.Tests.Customers.ValueObjects
+{
+    public class CustomerName_Test
+    {
+        private readonly Customer _customer;
+        public CustomerName_Test()
+        {
+            _customer = CustomerMoq.GetDefaultCustomer();
+        }
+
+        [Fact]
+        public void Should_have_fname_and_lname()
+        {
+            Assert.NotNull(_customer.Name.FirstName);
+            Assert.NotNull(_customer.Name.LastName);
+        }
+    }
+}
