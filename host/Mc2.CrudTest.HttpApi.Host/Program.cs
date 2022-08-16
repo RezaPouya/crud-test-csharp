c7385a022860f42d9b104171e87e216b7a51ffae
Reza P <rpouya@hotmail.com>
2022-08-16 23:01:00 +0430

    Register applicationdbContext in httpApiHost

--- host/Mc2.CrudTest.HttpApi.Host/Program.cs
+++ host/Mc2.CrudTest.HttpApi.Host/Program.cs
@@ -1,6 +1,13 @@
+using Mc2.CrudTest.Domain.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
