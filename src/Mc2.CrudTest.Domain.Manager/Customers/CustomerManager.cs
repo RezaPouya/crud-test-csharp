8cbecb381efeec14a0b8fad57d44c7725e5d76b3
Reza P <rpouya@hotmail.com>
2022-08-17 02:04:39 +0430

    Now we can create a customer

--- src/Mc2.CrudTest.Domain.Manager/Customers/CustomerManager.cs
+++ src/Mc2.CrudTest.Domain.Manager/Customers/CustomerManager.cs
@@ -1,5 +1,6 @@
 ﻿using Mc2.CrudTest.Domain.Customers;
 using Mc2.CrudTest.Domain.Customers.DTOs.InputDtos;
+using Mc2.CrudTest.Domain.Customers.ValueObjects;
 using Mc2.CrudTest.Domain.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,16 @@ namespace Mc2.CrudTest.Domain.Manager.Customers
 
             if (await IsEmailExistAsync(inputDto.Email))
                 throw new CustomerException("There is a customer with same email.");
+
+            var customerPhoneNumber = new CustomerPhoneNumber(inputDto.PhoneNumber.Trim());
+
+            Customer customer = new Customer(inputDto.Email.Trim(),
+                CreateCustomerPersonalInfo(inputDto)
+                , customerPhoneNumber,
+                inputDto.BankAccountNumber);
+
+            _dbContext.Customers.Add(customer);
+            await _dbContext.SaveChangesAsync(cancellationToken);
         }
 
         public Task DeleteAsync(string email, CancellationToken cancellationToken = default)
@@ -36,6 +47,11 @@ namespace Mc2.CrudTest.Domain.Manager.Customers
         private async Task<bool> IsEmailExistAsync(string email, CancellationToken cancellationToken = default)
         {
             return await _dbContext.Customers.AsNoTracking().AnyAsync(p => p.Email.Equals(email.Trim()), cancellationToken);
+        }
+
+        private CustomerPersonalInfo CreateCustomerPersonalInfo(CustomerInputDto inputDto)
+        {
+            return new CustomerPersonalInfo(inputDto.FirstName.Trim(), inputDto.LastName.Trim(), inputDto.DateOfBirth);
         }
     }
 }
--- test/Mc2.CrudTest.Domain.Manager.Tests/CustomerManager_Test.cs
+++ test/Mc2.CrudTest.Domain.Manager.Tests/CustomerManager_Test.cs
@@ -1,8 +1,10 @@
 ﻿using Mc2.CrudTest.Domain.Customers;
 using Mc2.CrudTest.Domain.Customers.DTOs.InputDtos;
+using Mc2.CrudTest.Domain.DataAccess;
 using Mc2.CrudTest.Domain.Manager.Customers;
 using Mc2.CrudTest.Domain.Manager.Tests.DataFixtures;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Mc2.CrudTest.Domain.Manager.Tests
@@ -10,10 +12,11 @@ namespace Mc2.CrudTest.Domain.Manager.Tests
     public class CustomerManager_Test : IClassFixture<ApplicationDbContextSeedDataFixture>
     {
         private ICustomerManager _manager;
-
-        public CustomerManager_Test(ApplicationDbContextSeedDataFixture fixture) 
+        private ApplicationDbContext _dbContext;
+        public CustomerManager_Test(ApplicationDbContextSeedDataFixture fixture)
         {
-            _manager = new CustomerManager(fixture._applicationDbContext);
+            _dbContext = fixture._applicationDbContext;
+            _manager = new CustomerManager(_dbContext);
         }
 
         [Fact]
@@ -36,6 +39,29 @@ namespace Mc2.CrudTest.Domain.Manager.Tests
             // assert
             var ex = Assert.Throws<CustomerException>(act);
             Assert.Contains("There is a customer with same email.", ex.Message);
+        }
+
+        [Fact]
+        public void should_create_customre()
+        {
+            // arrange
+            var inputDto = new CustomerInputDto()
+            {
+                Email = "a@sample.com",
+                BankAccountNumber = "IR0000001",
+                FirstName = "Mr.Z",
+                LastName = "Developer",
+                DateOfBirth = DateTime.Now.AddYears(-20),
+                PhoneNumber = "+989383810430"
+            };
+
+            // act
+            _manager.CreateAsync(inputDto).GetAwaiter().GetResult();
+
+            var customer = _dbContext.Customers.FirstOrDefault(p => p.Email.Equals(inputDto.Email));
+
+            // assert
+            Assert.NotNull(customer);
         }
     }
 }
