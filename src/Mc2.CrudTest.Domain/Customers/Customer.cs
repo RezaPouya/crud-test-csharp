6ba65459df36f368bf945a8751c9385fcc829a44
r.pouya <r.pouya@R-POUYA>
2022-08-16 10:05:25 +0430

    Now Customer has phone number

--- src/Mc2.CrudTest.Domain/Customers/Customer.cs
+++ src/Mc2.CrudTest.Domain/Customers/Customer.cs
@@ -11,14 +11,19 @@ namespace Mc2.CrudTest.Domain.Customers
         protected Customer()
         { }
 
-        public Customer(CustomerName name, CustomerDateOfBirth birthDate)
+        public Customer(CustomerName name, 
+            CustomerDateOfBirth birthDate,
+            CustomerPhoneNumber phoneNumber)
         {
             Name = name;
             DateOfBirth = birthDate;
+            PhoneNumber = phoneNumber;
         }
 
         public virtual CustomerName Name { get; protected set; }
 
         public virtual CustomerDateOfBirth DateOfBirth { get; protected set; }  
+
+        public virtual CustomerPhoneNumber PhoneNumber { get; protected set; }
     }
 }
--- test/Mc2.CrudTest.Domain.Tests/MoqObjects/CustomerMoq.cs
+++ test/Mc2.CrudTest.Domain.Tests/MoqObjects/CustomerMoq.cs
@@ -8,12 +8,14 @@ namespace Mc2.CrudTest.Domain.Tests.MoqObjects
         private static CustomerName _name;
         private static Customer _customer;
         private static CustomerDateOfBirth _dateOfBirth;
+        private static CustomerPhoneNumber _phoneNumber;
 
         public static Customer GetDefaultCustomer()
         {
-            _name = new CustomerName("Reza","Pouya");
+            _name = new CustomerName("Reza", "Pouya");
             _dateOfBirth = new CustomerDateOfBirth(System.DateTime.Now.AddYears(-32));
-            _customer = new Customer(_name , _dateOfBirth);
+            _phoneNumber = new CustomerPhoneNumber();
+            _customer = new Customer(_name, _dateOfBirth, _phoneNumber);
 
             return _customer;
         }
