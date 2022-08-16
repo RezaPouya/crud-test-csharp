558f29bbbae076d6c67899f95c3b69c504bf7e2f
Reza P <rpouya@hotmail.com>
2022-08-16 23:17:17 +0430

    Create ICustomerRepository and CustomerManager

---
+++ src/Mc2.CrudTest.Domain/Customers/ICustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.Customers
+{
+    public interface ICustomerRepository 
+    {
+        Task<List<Customer>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task<Customer> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+
+        Task Create(Customer customer, CancellationToken cancellationToken = default); 
+
+        Task Update(Customer customer, CancellationToken cancellationToken = default);
+
+        Task DeleteAsync(string email, CancellationToken cancellationToken = default); 
+    }
+}
