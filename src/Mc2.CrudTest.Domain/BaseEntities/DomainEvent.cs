786a0f0467b7adb0c395b7d577ed607ce41044fc
Reza P <rpouya@hotmail.com>
2022-08-17 23:28:57 +0430

    update DomainEvent base entity to be able to use mediatR Inotication marker

--- src/Mc2.CrudTest.Domain/BaseEntities/DomainEvent.cs
+++ src/Mc2.CrudTest.Domain/BaseEntities/DomainEvent.cs
@@ -1,19 +1,19 @@
-﻿namespace Mc2.CrudTest.Domain.BaseEntities
+﻿using MediatR;
+
+namespace Mc2.CrudTest.Domain.BaseEntities
 {
     [Serializable]
-    public class DomainEvent
+    public abstract class DomainEvent : INotification
     {
-        public DomainEvent(object obj ,  int eventOrder = 0)
+        public DomainEvent(int eventOrder = 0)
         {
             EventId = Guid.NewGuid();
             EventDateTime = DateTime.Now;
             EventOrder = eventOrder;
-            EventData = obj;
         }
 
         public Guid EventId { get; protected set; }
         public DateTime EventDateTime { get; protected set; }
-        public object EventData { get; protected set; }
         public int EventOrder { get; protected set; }
     }
 }
