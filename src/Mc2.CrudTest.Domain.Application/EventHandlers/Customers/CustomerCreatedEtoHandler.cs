5da401566cf2b1c9ac55fbc7a5a704ab9a7932c5
Reza P <rpouya@hotmail.com>
2022-08-18 02:08:02 +0430

    add event handlers

---
+++ src/Mc2.CrudTest.Domain.Application/EventHandlers/Customers/CustomerCreatedEtoHandler.cs
@@ -0,0 +1,23 @@
+﻿using Mc2.CrudTest.Domain.Customers.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Mc2.CrudTest.Domain.Application.EventHandlers.Customers
+{
+    public class CustomerCreatedEtoHandler : INotificationHandler<CustomerCreatedEto>
+    {
+        private readonly ILogger<CustomerCreatedEtoHandler> _logger;
+
+        public CustomerCreatedEtoHandler(ILogger<CustomerCreatedEtoHandler> logger)
+        {
+            this._logger = logger;
+        }
+
+        public Task Handle(CustomerCreatedEto notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Domain Event: {DomainEvent}", notification.GetType().Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Mc2.CrudTest.Domain.Application/EventHandlers/Customers/CustomerDeletedEtoHandler.cs
@@ -0,0 +1,18 @@
+﻿using Mc2.CrudTest.Domain.Customers.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Mc2.CrudTest.Domain.Application.EventHandlers.Customers
+{
+    public class CustomerDeletedEtoHandler : INotificationHandler<CustomerDeletedEto>
+    {
+        private readonly ILogger<CustomerDeletedEtoHandler> _logger;
+
+        public Task Handle(CustomerDeletedEto notification, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Domain Event: {DomainEvent}", notification.GetType().Name);
+
+            return Task.CompletedTask;
+        }
+    }
+}
