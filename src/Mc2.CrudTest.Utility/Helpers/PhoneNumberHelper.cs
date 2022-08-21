199e39cfd6eea8ee14fc7b93c21b7303978f6f91
Mason Chase <mason.chase@smartmed.world>
2022-08-21 19:08:20 +0200

    Fixed Mobile Validation

--- src/Mc2.CrudTest.Utility/Helpers/PhoneNumberHelper.cs
+++ src/Mc2.CrudTest.Utility/Helpers/PhoneNumberHelper.cs
@@ -5,18 +5,20 @@ namespace Mc2.CrudTest.Utility.Helpers
 {
     public static class PhoneNumberHelper
     {
+        private static readonly PhoneNumberUtil PhoneNumberUtil = PhoneNumberUtil.GetInstance();
+
         public static bool IsValidCellphoneNumber(string phoneNumber)
         {
             try
             {
-                var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+                
+                PhoneNumber phone = PhoneNumberUtil.Parse(phoneNumber.Trim(),
+                    CountryCodeSource.UNSPECIFIED.ToString());
 
-                PhoneNumber phone = phoneNumberUtil.Parse(phoneNumber.Trim(), CountryCodeSource.UNSPECIFIED.ToString());
-
-                return phoneNumberUtil.GetNumberType(phone) == PhoneNumberType.MOBILE ||
-                       phoneNumberUtil.GetNumberType(phone) == PhoneNumberType.FIXED_LINE_OR_MOBILE;
+                return PhoneNumberUtil.GetNumberType(phone) == PhoneNumberType.MOBILE ||
+                       PhoneNumberUtil.GetNumberType(phone) == PhoneNumberType.FIXED_LINE_OR_MOBILE;
             }
-            catch
+            catch(NumberParseException)
             {
                 return false;
             }
